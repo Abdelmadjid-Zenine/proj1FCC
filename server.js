e74eae3b568921b0d1ac8f45d02eac2dc4670a4b
Rayan <abdelmadjidzenine@gamil.com>
2022-04-06 13:54:14 +0100

    First commit

--- server.js
+++ server.js
@@ -3,17 +3,23 @@
 
 // init project
 var express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -24,6 +30,38 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  const inputDate = new Date(req.params.date)
+  Date.prototype.isValid = () => {
+    return inputDate.getTime() === inputDate.getTime()
+  }
+  if (!req.params.date) {
+    res.json({
+      "unix": new Date().getTime(),
+      "utc": new Date().toUTCString()
+    })
+  }
+  else{
+    if(inputDate.isValid())
+    {
+      
+      res.status(200).json({
+        "unix":inputDate.getTime(),
+        "utc":inputDate.toUTCString()
+      })
+    }
+    else{
+      res.json(
+        {
+          "error":"Invalid date"
+        }
+      )
+    }
+  }
+  
+  // const inputDate = new Date()
+ 
+});
 
 
 // listen for requests :)
